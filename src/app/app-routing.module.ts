cd125ebdacdb4706cae6ab805d96e154129c7684
Dannerkalo <Dannerkalo@gmail.com>
2020-01-12 21:00:45 +0100

    A new user add form is created and angular material date picker is initialized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from './components/users/users.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
 
 
 const routes: Routes = [
   {path:'users', component: UsersComponent},
+  { path: 'adduser', component: UserFormComponent }
  //{ path: 'users/:userId', component: UsersComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
-
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRippleModule,
+    MatInputModule,
+    MatTableModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
